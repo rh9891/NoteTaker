b74296eaf99ac3573c7d18e6361501d4b0f03411
Romie Hecdivert <artemis@Romies-MacBook-Pro-Artemis.local>
2020-07-08 09:27:26 -0400

    Added global variables, global actions for app.use, and a few routes.

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+const fs = require("fs");
+
+// Sets up the Express application to handle data parsing.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.use(express.static("public"));
+
+// * GET `/notes` - Should return the `notes.html` file.
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// * GET `*` - Should return the `index.html` file
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// * GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
+app.get("/api/notes", (req, res) => {
+    res.readFile(path.join(__dirname, "/db/db.json"));
+});
+
+
+// * POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+
+app.post("/api/notes", (req, res) => {
+
+});
+
+
+// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.
+// This means you'll need to find a way to give each note a unique `id` when it's saved. 
+// In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+app.delete("/api/notes/:id", (req, res) => {
+
+});
