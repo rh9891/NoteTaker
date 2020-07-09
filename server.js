f6756f05d3fa31135501167ff49874ad6486f517
Romie Hecdivert <artemis@Romies-MacBook-Pro-Artemis.local>
2020-07-09 10:37:06 -0400

    Fixed multiple issues with the file paths in both the functions and files.

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var notesArray = [];
 // Sets up the Express.js application to handle data parsing.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/Develop/public")));
+
 // Routes
 // =============================================================
 
@@ -21,18 +21,23 @@ app.use(express.static("public"));
 
 // This route returns the "notes.html" file.
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // This route reads the "db.json" file and returns all the saved notes as JSON.
 app.get("/api/notes", (req, res) => {
-    res.readFile(path.join(__dirname, "/db/db.json"));
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
+    console.log(data);
+    // Creates a variable to get information from the "db.json" file, which is the array of notes. FS reads things as string, so JSON.parse() is needed to convert.
+    notesArray = JSON.parse(data);
+    res.json(notesArray);
+    });
 });
 
 // This is the route that receives a new note to save on the request body, adds it to the "db.json" file, and then returns the new note to the user.
 app.post("/api/notes", (req, res) => {
-    console.log("save routes")
-    fs.readFile("/db/db.json", (err, data) => {
+    console.log("Save routes are locating array of notes.")
+    fs.readFile(path.join(__dirname, "/db/db.json"), (err, data) => {
         console.log(data);
         // Creates a variable to get information from the "db.json" file, which is the array of notes. FS reads things as string, so JSON.parse() is needed to convert.
         notesArray = JSON.parse(data);
@@ -45,25 +50,23 @@ app.post("/api/notes", (req, res) => {
     notesArray.push(newNote);
   
     // To write the file, the array of notes is passed as the second argument. FS reads things as string, so JSON.stringify() is needed to convert.
-    fs.writeFile("/db/db.json", JSON.stringify(notesArray), (err) => {
+    fs.writeFile("db/db.json", JSON.stringify(notesArray), (err) => {
         if (err) throw err;
         console.log("New note has been saved!");
         res.json(newNote);
       });
   });
 
-// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.
-// This means you'll need to find a way to give each note a unique `id` when it's saved. 
-// In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+// This route receives a query parameter containing the id of a note to delete. Each note is given a unique `id` when it is saved. In order to delete a note, this route reads all notes from the `db.json` file, removes the note with the given `id` property, and then rewrites the notes to the `db.json` file.
 app.delete("/api/notes/:id", (req, res) => {
-    fs.readFile("/db/db.json", (err, data) => {
+    fs.readFile("db/db.json", (err, data) => {
         console.log(data);
         // Creates a variable to get information from the "db.json" file, which is the array of notes. FS reads things as string, so JSON.parse() is needed to convert.
         notesArray = JSON.parse(data);
       });
     
     var deletedNote = req.params.id;
-    console.log("id: ",deletedNote);
+    console.log("id: ", deletedNote);
     
     notesArray = notesArray.filter((note) => {
         return note.id != deletedNote;
@@ -71,15 +74,15 @@ app.delete("/api/notes/:id", (req, res) => {
 
     console.log(notesArray)
 
-    fs.writeFile("./db/db.json", JSON.stringify(deletedNotes), (err) => {
+    fs.writeFile("db/db.json", JSON.stringify(deletedNote), (err) => {
         if (err) throw err;
-        res.json(deletedNotes);
+        res.json(deletedNote);
     });
 });
 
 // This is the "catch-all" route that returns the user to the "index.html" file. It should display on the bottom as the last get route.
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/Develop/public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // Starts the server to begin listening on the port.
